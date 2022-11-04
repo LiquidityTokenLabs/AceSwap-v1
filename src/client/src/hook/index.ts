13df83bdd67a0f9e2aeef41da1de59ed77da8cb2
Kazuma.I <kazuma.work77@gmail.com>
2022-11-05 02:30:59 +0900

    Fix: 修正

--- src/client/src/hook/index.ts
+++ src/client/src/hook/index.ts
@@ -1,6 +1,7 @@
 import { GoerliConfig } from '@liqlab/utils/Config/ContractConfig'
 import { Contract, ethers, utils } from 'ethers'
-import ROUTER_ABI from '@liqlab/contracts/artifacts/contracts/Router.sol/Router.json'
+// import ROUTER_ABI from '@liqlab/contracts/artifacts/contracts/Router.sol/Router.json'
+import ROUTER_ABI from '../../../contracts/artifacts/contracts/Router.sol/Router.json'
 import POOL721_ABI from '@liqlab/contracts/artifacts/contracts/pool/SingleNativeOPool.sol/SingleNativeOPool.json'
 import { Router } from '../../contracts/Router.sol'
 import { Pool721 } from '../../contracts/Pool721.sol'
